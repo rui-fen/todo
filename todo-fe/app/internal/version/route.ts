55a1b812f685a525c4a45a1a31c46d7a358eb9f1
chenfanda <fanda.hk2013@gmail.com>
2026-03-30 11:58:35 +0800

    test fe:update ci for vercel v4

--- todo-fe/app/internal/version/route.ts
+++ todo-fe/app/internal/version/route.ts
@@ -1,7 +1,7 @@
-import pkg from "../../../package.json"
+import packageJSON from "../../../package.json"
 
 export async function GET() {
-  return new Response(JSON.stringify({ version: pkg.version }), {
+  return new Response(JSON.stringify({ version: packageJSON.version }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   })
