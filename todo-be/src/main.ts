2eac1c7a816bc7a5a5277f7670be03248b1e56b2
chenfanda <fanda.hk2013@gmail.com>
2026-03-30 08:37:28 +0800

    fix: listen on 0.0.0.0

--- todo-be/src/main.ts
+++ todo-be/src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
